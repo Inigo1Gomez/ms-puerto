c32c51beb366f8ce1544e44af09f16fb2a616884
Inigo1Gomez <inigobenjamin2425@gmail.com>
2026-06-05 15:10:53 -0400

    .

--- src/main/java/com/CordyTech/Puerto/config/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.CordyTech.Puerto.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfig {
-    // Security removed: authentication/authorization must be handled centrally
-}
